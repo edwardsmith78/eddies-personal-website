c2512ef3275695090a5367dd464b7b3c77ddef5d
Edward Smith <esmith78@gmail.com>
2023-05-10 12:45:08 -0400

    updated html, style.css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "I'm orginally from Georgia";
+h2.textContent = "A few of my favorite things";
 
 document.querySelector("body").appendChild(h2);
